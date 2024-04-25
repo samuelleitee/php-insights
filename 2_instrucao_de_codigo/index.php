c0da9198e11f1218d874d816e60b6d6769689be7
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 18:40:26 -0300

    feat: #2 instrução de código

---
+++ 2_instrucao_de_codigo/index.php
@@ -0,0 +1,10 @@
+<?php 
+    // Em instruções de apenas uma linha, é necessário acrescentar o ";" ao final.
+    echo "Instrução de uma linha <br>";
+    echo "Outra instrução de uma linha <br>";
+
+    // Em instruções de mais de uma linha, não é necessário acrescentar o ";" ao final.
+    if(5 > 2) {
+        echo "Dentro do if";
+    }
+?>
