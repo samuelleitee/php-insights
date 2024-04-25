bdc926b2e84a8cae45a23b04a84c1f675b2a2d30
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 18:41:10 -0300

    feat: #4 instrução de código

---
+++ 4_comentarios/index.php
@@ -0,0 +1,15 @@
+<!-- Comentários 
+    Servem para dar informações e direções importantes
+    Iniciam-se com //, # ou /* */
+    O conteúdo dos comentários é ignorado durante a execução
+
+    OBS: não se deve incluir informações sensíveis aos comentários
+ -->
+
+<?php
+    # Este é um comentário
+    // Este é um comentário 
+    /* Este é um comentário multi-linha */
+
+    echo "Teste comentário";
+?>
