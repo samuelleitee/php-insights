86662cec019d6ce582318e454a8a8d834d97026b
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 18:41:34 -0300

    feat: adicionar comentários descritivos

---
+++ ex2.php
@@ -0,0 +1,17 @@
+<!-- Exercício 2
+    Crie um arquivo PHP
+    Este arquivo deve conter uma descrição das características básicas de uma pessoa ou objeto utilizando comentários
+    Pelo menos três em linhas separadas
+    Execute o arquivo
+-->
+
+<?php 
+    // Esta variável contém o nome da pessoa
+    $nome = "Samuel";
+
+    # Esta variável contém a idade da pessoa
+    $idade = 29;
+
+    /* Esta variável contém a profissão da pessoa */
+    $profissao = "Programador";
+?>
