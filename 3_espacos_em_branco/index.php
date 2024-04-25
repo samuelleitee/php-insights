2de4e2e2fad719278c2ad54664bc192b51e7d269
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 18:40:51 -0300

    feat: #3 espaços em branco

---
+++ 3_espacos_em_branco/index.php
@@ -0,0 +1,8 @@
+<?php 
+    // Quebras de linha e espaços em brano são ignorados quando o código é processado.
+    echo          "Testando espaço em branco <br>";
+    echo "Testando espaço <br>";
+    echo 
+        "Quebra de linha <br>"
+    ;
+?>
