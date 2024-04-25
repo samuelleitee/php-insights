6251ff4bbeef31095e1d37716d4074dbfe676f75
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 18:05:53 -0300

    feat: show your name on the screen

---
+++ ex1.php
@@ -0,0 +1,9 @@
+<!-- Exercício 1
+    Crie um arquivo PHP
+    Este arquivo deve conter uma instrução que imprime o seu nome
+    Execure o arquivo
+-->
+
+<?php 
+    echo "Samuel Leite Pereira"
+?>
