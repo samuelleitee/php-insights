186229669a6adb0ee862b563a3bf53d8f2468a33
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 19:04:27 -0300

    feat: #5 palavras reservadas

---
+++ 5_palavras_reservadas/index.php
@@ -0,0 +1,15 @@
+<!-- Palavras Reservadas 
+    Instruções que já existem no programa e não podem ser utilizadas em nomes de variáveis ou funções
+
+    Ex.: echo, insteadof, else, interface, namespace, pow, __DIR__, __FILE__, endif, print, private, protected, and, require, public, as, break, case, for, finally, switch, throw e etc.
+-->
+
+<?php 
+    // function echo() {
+    //     return true;
+    // } ERROR
+
+    function myFunction() {
+        return true;
+    }
+?>
