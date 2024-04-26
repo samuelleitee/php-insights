5de93053b0e548798b84a01841248dc1c0087c7e
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 22:13:05 -0300

    feat: tipos de dados - inteiros

---
+++ tipos_de_dados/1_inteiros/index.php
@@ -0,0 +1,14 @@
+<?php
+    echo 5;
+    echo "<br>";
+    echo 5 + 7;
+    echo "<br>";
+    echo -12;
+    echo "<br>";
+    echo 5 -12;
+    
+    $numero = 5
+    
+    echo "<br>";
+    echo $numero;
+?>
