9b2e9900b02a8e7713576f7834d50bb29a6a932b
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 22:14:45 -0300

    feat: tipos de dados - arrays

---
+++ tipos_de_dados/9_arrays/index.php
@@ -0,0 +1,7 @@
+<?php
+    $meuArray = [1, 2, 3];
+
+    // echo $meuArray; <- Retorna um erro pois o comando echo tenta converter o array para String
+    print_r($meuArray);
+    echo $meuArray[0]
+?>
