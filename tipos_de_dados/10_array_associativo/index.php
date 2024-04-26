a2420de5b31566edbcbb358165116ce34dbe5fec
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 22:12:29 -0300

    feat: tipos de dados - array associado

---
+++ tipos_de_dados/10_array_associativo/index.php
@@ -0,0 +1,13 @@
+<?php
+    // Array com chave e valor
+
+    $arr = ["nome" => "Shark", "patas" => 4, "cor" => "Marrom"];
+
+    echo $arr["nome"];
+    echo "<br>";
+    print_r($arr);
+    echo "<br>";
+
+    $arrAssoc = ["chave" => "valor", "bool" => true];
+    print_r($arrAssoc);
+?>  
