493a7bf8c7c141a845e6c4bad9b468a0abb738f0
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 22:13:52 -0300

    feat: tipos de dados - strings

---
+++ tipos_de_dados/5_strings/index.php
@@ -0,0 +1,9 @@
+<?php 
+    echo "Testanto texto de aspas duplas <br>";
+    echo "Testanto texto de aspas simples <br>";
+
+    $idade = 15;
+
+    // Aspas duplas interpretam variáveis
+    echo "Ele tem $idade <br>";
+?>
