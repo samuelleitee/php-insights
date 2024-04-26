8681005ee2f41c1947562f964c5be22a53701576
samuelleitee <programmer.and.investor@gmail.com>
2024-04-25 22:15:42 -0300

    feat: Crie um aray com as características de um carro

---
+++ ex6.php
@@ -0,0 +1,21 @@
+<!-- Exercício 6
+    Crie um aray com as características de um carro
+-->
+
+<?php
+    $carro = [
+        "marca" => "BMW",
+        "rodas" => 4,
+        "teto_solar" => true,
+        "velocidade_max" => 300,
+        "blindado" => false,
+    ];
+
+    print_r($carro);
+
+    $marca = $carro["marca"];
+    $velocidade_maxima = $carro["velocidade_max"];
+
+    echo "<br>";
+    echo "O carro é da marca $marca e atinge no maximo $velocidade_maxima km/h";
+?>
